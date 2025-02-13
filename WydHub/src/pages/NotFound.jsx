96c78dafdfc9301d6d902cccb4f5254e8f48a8e3
devlucasmattos <lucasmattos312@gmail.com>
2025-02-12 23:31:00 -0300

    Adicionado melhoria na página 404

--- WydHub/src/pages/NotFound.jsx
+++ WydHub/src/pages/NotFound.jsx
@@ -3,8 +3,9 @@ import "./NotFound.css";
 const NotFound = () => {
   return (
     <div className="not-found-container">
+      <img src="./images/imagem404.png" alt="Página não encontrada" className="not-found-image" />
       <h1 className="not-found-title">404 - Página não encontrada</h1>
-      <p className="not-found-text">O caminho digitado não existe.</p>
+      <p className="not-found-text">Procurando um drop específico? Não é por aqui.</p>
       <a href="/" className="not-found-link">
         Voltar para Home
       </a>
