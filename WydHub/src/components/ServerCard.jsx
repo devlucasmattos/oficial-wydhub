8728a99c00cbc0d8c5c716e132a3267f35edd594
devlucasmattos <lucasmattos312@gmail.com>
2025-02-13 21:40:41 -0300

    Realizada correção na exibição dos itens do ServerCard.jsx

--- WydHub/src/components/ServerCard.jsx
+++ WydHub/src/components/ServerCard.jsx
@@ -1,31 +1,28 @@
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
-import servers from "../Data"; // Importando os servidores diretamente
 import "./ServerCard.css";
 
-const ServerCard = () => {
+const ServerCard = ({ server }) => {
   const navigate = useNavigate();
 
-  // Garante que 'servers' seja sempre um array, mesmo que seja undefined
-  const validServers = Array.isArray(servers) ? servers : [];
-
   return (
-    <div className="server-card-container">
-      {validServers.map((server) => (
-        <div key={server.id} className="server-card" onClick={() => navigate(`/server/${server.id}`)}>
-          <img src={`${import.meta.env.BASE_URL}${server.image}`} alt={server.name} className="server-image" />
-          <div className="server-info">
-            <h3 className="server-name">{server.name}</h3>
-            <p className="server-description">{server.description}</p>
-          </div>
-        </div>
-      ))}
+    <div className="server-card" onClick={() => navigate(`/server/${server.id}`)}>
+      <img src={`${import.meta.env.BASE_URL}${server.image}`} alt={server.name} className="server-image" />
+      <div className="server-info">
+        <h3 className="server-name">{server.name}</h3>
+        <p className="server-description">{server.description}</p>
+      </div>
     </div>
   );
 };
 
 ServerCard.propTypes = {
-  servers: PropTypes.array,
+  server: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default ServerCard;
